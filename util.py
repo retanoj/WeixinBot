4ba10ce7d03b9abf4b53c3da08692e1b3bc34e39
retanoj <s919student@gmail.com>
2016-08-29 22:51:06 +0800

    project

---
+++ util.py
@@ -0,0 +1,41 @@
+# coding:utf-8
+import logging
+
+ME = '张小炮'
+
+def catchKeyboardInterrupt(fn):
+    def wrapper(*args):
+        try:
+            return fn(*args)
+        except KeyboardInterrupt:
+            logging.debug('[*] 强制退出程序')
+    return wrapper
+
+
+def _decode_list(data):
+    rv = []
+    for item in data:
+        if isinstance(item, unicode):
+            item = item.encode('utf-8')
+        elif isinstance(item, list):
+            item = _decode_list(item)
+        elif isinstance(item, dict):
+            item = _decode_dict(item)
+        rv.append(item)
+    return rv
+
+
+def _decode_dict(data):
+    rv = {}
+    for key, value in data.iteritems():
+        if isinstance(key, unicode):
+            key = key.encode('utf-8')
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+        elif isinstance(value, list):
+            value = _decode_list(value)
+        elif isinstance(value, dict):
+            value = _decode_dict(value)
+        rv[key] = value
+    return rv
+
